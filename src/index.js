ec2a30067638bd9761a5fea160626b78e6c8f8ce
light86neon <72137342+light86neon@users.noreply.github.com>
2021-05-25 16:17:14 +0300

    react components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
-const el = (
-    <>
-    <h1>My Todo List</h1>
-        <input placeholder="search"/>
+const TodoList = () => {
+    return (
         <ul>
             <li>Learn React</li>
             <li>Build Awesome App</li>
             <li>Learn Redux</li>
         </ul>
-    </>
-);
+    )
+}
 
+const AppHeader = () => {
+    return (
+        <h1>My Todo List</h1>
+    )
+}
 
-    ReactDom.render(el, document.getElementById('root'));
+const SearchPanel = () => {
+    return <input placeholder="search"/>
+}
+
+const App = () => {
+    return (
+        <>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </>
+
+    )
+}
+ReactDom.render(<App/>, document.getElementById('root'));
