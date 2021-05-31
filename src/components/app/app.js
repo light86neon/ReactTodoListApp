9dc75d99d3c06b11ad3d7bbe6442f27d2d8e42b6
light86neon <72137342+light86neon@users.noreply.github.com>
2021-05-31 15:50:30 +0300

    finished

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,9 @@ export default class App extends Component {
             this.createTodoItem('Learn React'),
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Learn Redux')
-        ]
+        ],
+        term: '',
+        filter: 'done'// active, all, done
     };
 
     toggleProperty(arr, id, propName) {
@@ -99,27 +101,57 @@ export default class App extends Component {
         });
     };
 
+    onSearchChange = (term) => {
+        this.setState({term})
+    };
+
+    onFilterChange = (filter) => {
+        this.setState({filter})
+    };
+    search(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+        return items.filter((item) => {
+            return item.label.toLowerCase()
+                .indexOf(term.toLowerCase()) > -1;
+        });
+    }
+
+    filter(items, filter) {
+        switch(filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
+    }
     render() {
 
-        const { todoData } = this.state;
+        const { todoData, term, filter } = this.state;
 
-        // filter create new array
+        const visibleItems = this.filter(
+            this.search(todoData, term), filter);
+
         const doneCount = todoData
-            //покаже кількість елементів done
             .filter((el)=> el.done).length;
-        // весь масив мінус кількість виконаних елементів це
-        // кількість елементів яких потрібно виконати
         const todoCount = todoData.length - doneCount;
 
         return (
             <div className="todo-app">
                 <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className='top-panel d-flex'>
-                    <SearchPanel/>
-                    <ItemStatusFilter/>
+                    <SearchPanel onSearchChange = {this.onSearchChange}/>
+                    <ItemStatusFilter filter = {filter}
+                        onFilterChange = {this.onFilterChange}
+                    />
                 </div>
 
-                <TodoList todos={todoData}
+                <TodoList todos={visibleItems}
                           onDeleted={this.deleteItem}
                           onToggleImportant = {this.onToggleImportant}
                           onToggleDone = { this.onToggleDone }
